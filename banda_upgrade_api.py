706295f7ddd71dedfe806499f12e4144985985ac
Gustavo <ggustavobedasi@gmail.com>
2020-06-30 12:19:59 -0300

    Atualização do output

--- banda_upgrade_api.py
+++ banda_upgrade_api.py
@@ -3,6 +3,9 @@ import requests
 def banda_request():
     banda_input = input("Digite a banda desejada: ")
     req = requests.post('https://nettools.club/_cisco_rlc_ajax', data = {'inspeed': banda_input})
-    print(req.text)
+    response = (req.text).split(' ')
+
+    print("[speed]  [normurst]  [extended burst]")
+    print(response[0], str(" " * 1) ,response[1], str(" " * 4) ,response[2])
 
 banda_request()
